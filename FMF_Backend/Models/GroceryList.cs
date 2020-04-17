cac52fb1cdc622db7c1a1e8fcedb9f775440e07e
Rune Alexander <alexander.laursen@gmail.com>
2020-04-17 11:39:54 +0200

    small changes

--- FMF_Backend/Models/GroceryList.cs
+++ FMF_Backend/Models/GroceryList.cs
@@ -1,9 +1,7 @@
 namespace FMF_Backend.Models{
     public class GroceryList {
         public long Id { get; set; }
-        public string ProductName { get; set; }
-        public double Price { get; set; }
-        public string Supplier { get; set; }
+        public Product ProductName { get; set; }
     
     }
 }
