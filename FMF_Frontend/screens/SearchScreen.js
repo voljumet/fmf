3ac4b02adb5dd17757d24449279b2608185fa341
Ole <gunvalds1@gmail.com>
2020-05-06 11:14:49 +0200

    prepared profilescreen for backend information

--- FMF_Frontend/screens/SearchScreen.js
+++ FMF_Frontend/screens/SearchScreen.js
@@ -35,7 +35,7 @@ export default class SearchScreen extends React.Component {
                      this.keyboardDidHide
                    );
                    // Change the URL to Store's URL
-                   return fetch("https://ffa7a10b.ngrok.io/api/product")
+                   return fetch("https://477b00dd.ngrok.io/api/product")
                      .then((response) => response.json())
                      .then((responseJson) => {
                        this.setState({
--- ngrok/README.txt
+++ ngrok/README.txt
@@ -2,7 +2,12 @@ to run, use terminal:
 ./ngrok http https://localhost:5001
 
 for help:
-./ngrok help 
+./ngrok help
 
 
 https://ngrok.com/docs
+
+
+To start backend:
+
+controll + F5
