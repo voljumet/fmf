86085c88c56ce415e3c132acfcc32272e5f6c49c
Peshang <peseng88@gmail.comn>
2020-05-20 13:12:02 +0200

    Rebased with develop, Folder name change

--- FMF_Frontend/screens/NewScreen.js
+++ FMF_Frontend/screens/NewScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ShoppingCart from '../Shopping/ShoppingCart';
 import { Provider } from 'react-redux';
-import store from '../Shopping/store';
+import store from '../../FMF_Frontend/Shopping/store';
 
 export default class NewScreen extends React.Component {
   render() {
