af512c15a9a837a132349990f1c59362e0c90195
Ole <gunvalds1@gmail.com>
2020-05-25 22:24:06 +0200

    attempt to retrieve googleid.

--- FMF_Backend/Data/FMFInitializer.cs
+++ FMF_Backend/Data/FMFInitializer.cs
@@ -17,11 +17,11 @@ namespace FMF_Backend.Data{
             context.Database.EnsureCreated();
 
             context.Profiles.AddRange(new List<Profile>{
-                new Profile("Rune Alexander","Laursen","Kristian IVs gate 17, 4612 Kristiansand", 93598, 7, "blablabla"),
-                new Profile("Ole","Gunvaldsen","Jon Lilletuns Vei 17, 4879 Grimstad", 93598, 7, "blablabla"),
-                new Profile("Anne Lise","Skjæveland","Lagerveien 12, 3030 Stavanger", 93598, 7, "blablabla"),
-                new Profile("Peshang","Alo","Venneslaveien 7, 4688 Vennesla", 93598, 7, "blablabla"),
-                new Profile("Morteza","Haidari","Tønnevoldsgate 44b, 4879 Grimstad", 93598, 7, "blablabla")
+                new Profile("Rune Alexander","Laursen","Kristian IVs gate 17, 4612 Kristiansand", 93598, 7, "blablabla", "1111111111"),
+                new Profile("Ole","Gunvaldsen","Jon Lilletuns Vei 17, 4879 Grimstad", 93598, 7, "blablabla", "22222222222"),
+                new Profile("Anne Lise","Skjæveland","Lagerveien 12, 3030 Stavanger", 93598, 7, "blablabla", "33333333333"),
+                new Profile("Peshang","Alo","Venneslaveien 7, 4688 Vennesla", 93598, 7, "blablabla", "44444444444"),
+                new Profile("Morteza","Haidari","Tønnevoldsgate 44b, 4879 Grimstad", 93598, 7, "blablabla", "555555555")
             });
 
             string store1 = new WebClient().DownloadString("https://my-json-server.typicode.com/voljumet/demo/Store1");
