479ec4d6b4328ec2bd37b89fb7f34dc94cd5200f
Anne Lise Skjaveland <annels16@student.uia.no>
2020-05-25 21:48:10 +0200

    Added fetch product from backend

--- FMF_Frontend/screens/ShopScreen.js
+++ FMF_Frontend/screens/ShopScreen.js
@@ -3,11 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import ShoppingCart from '../Shopping/ShoppingCart';
 import { Provider } from 'react-redux';
 import store from '../Shopping/store';
+import { Header } from "react-native-elements";
+
 
 export default class NewScreen extends React.Component {
   render() {
+    const {navigate} = this.props.navigation;
     return (
       <Provider store={store}>
+        <Header
+        leftComponent={{ icon: 'home', color: '#fff', onPress: () => navigate("Home")}}
+        centerComponent={{ text: 'Create shopping list', style: { color: '#fff' } }}
+        rightComponent={{ icon: 'person', color: '#fff', onPress: () => navigate("Profile")}}
+        />
         <ShoppingCart />
       </Provider>
     );
