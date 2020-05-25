38e8f3b00633c7fe6570c141064e0e994e8ac704
Anne Lise Skjaveland <annels16@student.uia.no>
2020-05-26 01:53:47 +0200

    added put request

--- FMF_Frontend/screens/MapScreen.js
+++ FMF_Frontend/screens/MapScreen.js
@@ -76,11 +76,11 @@ componentDidMount = async () => {
 
   liste = []
 
- await fetch(" https://659cad17.ngrok.io/api/orderList/")
+ await fetch("http://7c753338.ngrok.io/api/orderList/")
                      .then((response) => response.json())
                      .then((responseJson) => {
                       for(const list of responseJson){ 
-                        fetch(" https://659cad17.ngrok.io/api/orderList/getOrderList/" + list.id)
+                        fetch("http://7c753338.ngrok.io/api/orderList/getOrderList/" + list.id)
                         .then((response) => response.json())
                         .then((resJson) => {
                           if(resJson.shopper != null || resJson.shopper != undefined && resJson.available){
